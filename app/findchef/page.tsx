7e589013a2007399f5ea086a1e56a2335377dd7f
zariyatyasin <zariyatyasin789@gmail.com>
2024-10-14 02:11:51 +0600

    yes

--- app/findchef/page.tsx
+++ app/findchef/page.tsx
@@ -27,6 +27,7 @@ export default async function Page() {
           </div>
         </div>
       </section>
+
       <section className="py-12 md:py-20 lg:py-24">
         <div className="container px-4 md:px-6">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
