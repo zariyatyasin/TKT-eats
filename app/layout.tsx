f12854d04c60140f2556fab915a7910cd360cd29
zariyatyasin <zariyatyasin789@gmail.com>
2024-10-15 10:47:42 +0600

    tkt pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,12 @@ import Footer from "@/components/footer/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from "@vercel/analytics/react";
+
 export const metadata: Metadata = {
   title: "The Kitchen Table",
   description: "DINE LUXURIOUSLY, YOUR WAY",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -19,8 +21,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1278917256575194'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1278917256575194&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={inter.className}>
-        {" "}
         {/* <Navbar /> */}
         <Header />
         {children}
