51c057bb098654214bfe65eac533a70283511311
zariyatyasin <zariyatyasin789@gmail.com>
2024-10-15 10:54:11 +0600

    tkt pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Footer from "@/components/footer/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "The Kitchen Table",
@@ -21,12 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
+      <body className={inter.className}>
+        <Script id="facebook-pixel">
+          {`
+            !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -36,9 +35,8 @@ export default function RootLayout({
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1278917256575194'); 
               fbq('track', 'PageView');
-            `,
-          }}
-        />
+          `}
+        </Script>
         <noscript>
           <img
             height="1"
@@ -47,9 +45,6 @@ export default function RootLayout({
             src="https://www.facebook.com/tr?id=1278917256575194&ev=PageView&noscript=1"
           />
         </noscript>
-        {/* End Meta Pixel Code */}
-      </head>
-      <body className={inter.className}>
         {/* <Navbar /> */}
         <Header />
         {children}
