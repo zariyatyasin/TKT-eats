1284db003311f61dbcc3d8bbcf47b7173c882f2b
zariyatyasin <zariyatyasin789@gmail.com>
2024-11-18 22:56:59 +0600

    new push sep

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,25 @@ import HomeDemo1 from "@/components/homedemo/homedemo1";
 import Navbar from "@/components/layout/navbar";
 import Image from "next/image";
 import { GetAllChef } from "./findchef/_utils/action";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Private Chef Services | The Kitchen Table",
+  description:
+    "Discover top private chefs for your events and dining experiences. Enjoy luxurious dining with our curated selection of private chefs.",
+  keywords: "private chef, dining, luxury, events, culinary, chef services",
+  openGraph: {
+    title: "Private Chef Services | The Kitchen Table",
+    description:
+      "Discover top private chefs for your events and dining experiences. Enjoy luxurious dining with our curated selection of private chefs.",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddrjnijut/image/upload/v1731948968/qafhp0whircrvs1ypfo3.png",
+        alt: "Private Chef Services",
+      },
+    ],
+  },
+};
 
 export default async function Home() {
   const result = await GetAllChef({ limit: "4" });
